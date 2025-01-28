9a8991a9b22ed7172af9bb8b66de781d4c0cd1ce
connerohnesorge <conneroisu@outlook.com>
2025-01-27 19:48:41 -0600

    latest

--- README.md
+++ README.md
@@ -2,3 +2,56 @@
 libsql zig
 
 orm-ish for those who know their schema at compile time.
+
+## usage
+
+### installation
+
+```sh
+zig fetch --save "git+https://github.com/conneroisu/libsqlz#v1.0.0"
+```
+
+Use `libsqlz` in your `build.zig` file:
+```zig
+// build.zig
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    // exe declartion ...
+
+    const libsqlz = b.dependency("libsqlz", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.root_module.addImport("libsqlz", libsqlz.module("libsqlz"));
+    
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // exe unit tests declaration ...
+    
+    exe_unit_tests.root_module.addImport(
+        "libsqlz",
+        libsqlz.module("libsqlz"),
+    );
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
+```
+
+### usage
--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
 
     devenv = {
       url = "github:cachix/devenv";
@@ -20,165 +20,147 @@
   outputs = {
     self,
     nixpkgs,
-    nixpkgs-unstable,
     devenv,
-    systems,
+    flake-utils,
     ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    packages = forEachSystem (system: {
-      devenv-up = self.devShells.${system}.default.config.procfileScript;
-      devenv-test = self.devShells.${system}.default.config.test;
-    });
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      inherit (pkgs.stdenv) isLinux isDarwin isAarch64;
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [];
+        config.allowUnfree = true;
+      };
+      rosettaPkgs =
+        if isDarwin && isAarch64
+        then pkgs.pkgsx86_64Darwin
+        else pkgs;
+    in {
+      packages = {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        devenv-test = self.devShells.${system}.default.config.test;
+      };
 
-    devShells =
-      forEachSystem
-      (system: let
-        inherit (pkgs.stdenv) isLinux isDarwin isAarch64;
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [];
-          config.allowUnfree = true;
-        };
-        pkgs-unstable = import nixpkgs-unstable {
-          inherit system;
-          overlays = [];
-          config.allowUnfree = true;
-        };
-        rosettaPkgs =
-          if isDarwin && isAarch64
-          then pkgs.pkgsx86_64Darwin
-          else pkgs;
-      in {
-        default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            {
-              #
-              devcontainer = {
+      devShells.default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          {
+            #
+            devcontainer = {
+              enable = true;
+              settings.customizations.vscode.extensions = [
+                "github.copilot"
+                "github.codespaces"
+                "ms-python.vscode-pylance"
+                "redhat.vscode-yaml"
+                "redhat.vscode-xml"
+                "visualstudioexptteam.vscodeintellicode"
+                "bradlc.vscode-tailwindcss"
+                "christian-kohler.path-intellisense"
+                "supermaven.supermaven"
+                "jnoortheen.nix-ide"
+                "mkhl.direnv"
+                "tamasfe.even-better-toml"
+                "eamodio.gitlens"
+                "streetsidesoftware.code-spell-checker"
+                "editorconfig.editorconfig"
+              ];
+            };
+
+            git-hooks = {
+              hooks = {
+                alejandra.enable = true;
+              };
+            };
+
+            languages = {
+              zig = {
                 enable = true;
-                settings.customizations.vscode.extensions = [
-                  "github.copilot"
-                  "github.codespaces"
-                  "ms-python.vscode-pylance"
-                  "redhat.vscode-yaml"
-                  "redhat.vscode-xml"
-                  "visualstudioexptteam.vscodeintellicode"
-                  "bradlc.vscode-tailwindcss"
-                  "christian-kohler.path-intellisense"
-                  "supermaven.supermaven"
-                  "jnoortheen.nix-ide"
-                  "mkhl.direnv"
-                  "tamasfe.even-better-toml"
-                  "eamodio.gitlens"
-                  "streetsidesoftware.code-spell-checker"
-                  "editorconfig.editorconfig"
-                ];
+                package = pkgs.zig;
               };
+              nix.enable = true;
+              c.enable = true;
+              rust.enable = true;
+            };
 
-              git-hooks = {
-                hooks = {
-                  alejandra.enable = true;
-                };
-              };
+            packages =
+              (with pkgs; [
+                tcl
+                gnum4
 
-              languages = {
-                zig = {
-                  enable = true;
-                  package = pkgs-unstable.zig;
-                };
-                nix.enable = true;
-                c.enable = true;
-                rust.enable = true;
-              };
+                watchexec
+                stdenv.cc
 
-              packages =
-                (with pkgs-unstable; [
-                  tcl
-                  gnum4
+                tbb # Intel Threading Building Blocks
+                llvmPackages.openmp # OpenMP support
+                binutils
+              ])
+              ++ (with pkgs; [
+                alejandra
+                libclang
+              ])
+              ++ pkgs.lib.optionals isLinux [
+                pkgs.xorg.libX11
+                pkgs.gdb
+              ]
+              ++ pkgs.lib.optionals isDarwin ((
+                  with pkgs; [
+                    darwin.apple_sdk.frameworks.CoreFoundation
+                    darwin.apple_sdk.frameworks.Security
+                    darwin.apple_sdk.frameworks.SystemConfiguration
+                    rosettaPkgs.gdb
+                  ]
+                )
+                ++ (
+                  with pkgs; [
+                    darwin.apple_sdk.frameworks.Foundation
+                    darwin.apple_sdk.frameworks.IOKit
+                  ]
+                ));
 
-                  watchexec
-                  stdenv.cc
+            enterShell = ''
 
-                  tbb # Intel Threading Building Blocks
-                  llvmPackages.openmp # OpenMP support
-                  binutils
-                ])
-                ++ (with pkgs; [
-                  alejandra
-                  libclang
-                ])
-                ++ pkgs.lib.optionals isLinux [
-                  pkgs.xorg.libX11
-                  pkgs.gdb
-                ]
-                ++ pkgs.lib.optionals isDarwin ((
-                    with pkgs; [
-                      darwin.apple_sdk.frameworks.CoreFoundation
-                      darwin.apple_sdk.frameworks.Security
-                      darwin.apple_sdk.frameworks.SystemConfiguration
-                      rosettaPkgs.gdb
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
+              export LD_LIBRARY_PATH=${
+                pkgs.lib.makeLibraryPath (
+                  (with pkgs; [
+                    pkgs.mesa
+                    stdenv.cc
+                  ])
+                  ++ (
+                    pkgs.lib.optionals isLinux [
+                      pkgs.xorg.libX11
                     ]
                   )
                   ++ (
-                    with pkgs-unstable; [
-                      darwin.apple_sdk.frameworks.Foundation
-                      darwin.apple_sdk.frameworks.IOKit
+                    pkgs.lib.optionals isDarwin [
                     ]
-                  ));
-
-              enterShell = ''
-
-                export REPO_ROOT=$(git rev-parse --show-toplevel)
-                export LD_LIBRARY_PATH=${
-                  pkgs.lib.makeLibraryPath (
-                    (with pkgs-unstable; [
-                      pkgs.mesa
-                      stdenv.cc
-                    ])
-                    ++ (with pkgs; [
-                      ])
-                    ++ (
-                      pkgs.lib.optionals isLinux [
-                        pkgs.xorg.libX11
-                      ]
-                    )
-                    ++ (
-                      pkgs.lib.optionals isDarwin [
-                        pkgs.darwin.apple_sdk.frameworks.CoreFoundation
-                        pkgs.darwin.apple_sdk.frameworks.Security
-                        pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-                        pkgs.darwin.apple_sdk.frameworks.Foundation
-                        pkgs.darwin.apple_sdk.frameworks.IOKit
-                      ]
-                    )
                   )
-                }:$LD_LIBRARY_PATH
+                )
+              }:$LD_LIBRARY_PATH
 
+            '';
+
+            scripts = {
+              dx.exec = ''
+                $EDITOR $REPO_ROOT/flake.nix
               '';
 
-              scripts = {
-                dx.exec = ''
-                  $EDITOR $REPO_ROOT/flake.nix
-                '';
+              build-libsqlc.exec = ''
+                cd ./vendor/libsql-c
+                sh build.sh
+                # TODO: auto copy build outputs
+              '';
 
-                build-libsqlc.exec = ''
-                  cd ./vendor/libsql-c
-                  sh build.sh
-                  # TODO: copy build outputs
-                '';
+              tests.exec = ''
+                rm -rf ./.zig-cache/
+                zig build test
+              '';
+            };
 
-                tests.exec = ''
-                  rm -rf ./.zig-cache/
-                  zig build test
-                '';
-              };
-
-              cachix.enable = true;
-            }
-          ];
-        };
-      });
-  };
+            cachix.enable = true;
+          }
+        ];
+      };
+    });
 }
